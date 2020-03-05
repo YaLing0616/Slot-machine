e2769eddaac93b83863ab83af5124c0acef9da9a
YaLing0616 <s1102b011@gmail.com>
2020-03-05 13:49:40 +0800

    Update：homepageUrl

---
+++ build/precache-manifest.0d774cc8d83685b1cf83cc22a53a0ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936ad9355cfb02a78d0c68eeacdb8c3c",
+    "url": "/Slot-machine/index.html"
+  },
+  {
+    "revision": "516619e716ca8b7eb9aa",
+    "url": "/Slot-machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb8da525dcd609d9040a",
+    "url": "/Slot-machine/static/js/2.b6e6d0d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Slot-machine/static/js/2.b6e6d0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516619e716ca8b7eb9aa",
+    "url": "/Slot-machine/static/js/main.8275ec8f.chunk.js"
+  },
+  {
+    "revision": "8b3ee2d81ed4c5b548c7",
+    "url": "/Slot-machine/static/js/runtime-main.8ae04204.js"
+  }
+]);
