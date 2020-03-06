fe7271d7a339c3db7c7f3602cf59f7d9568ec53a
YaLing0616 <s1102b011@gmail.com>
2020-03-06 10:56:36 +0800

    Updates

---
+++ precache-manifest.c844ffeb0cc9e3ce61e9c263531cc035.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b13424adabde434c862aaaad78d6b0",
+    "url": "/Slot-machine/index.html"
+  },
+  {
+    "revision": "9eef552bde7793d64d96",
+    "url": "/Slot-machine/static/css/main.51bf6686.chunk.css"
+  },
+  {
+    "revision": "35a5a096c7be4f451436",
+    "url": "/Slot-machine/static/js/2.afc366f6.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/Slot-machine/static/js/2.afc366f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eef552bde7793d64d96",
+    "url": "/Slot-machine/static/js/main.f86a2a9f.chunk.js"
+  },
+  {
+    "revision": "8b3ee2d81ed4c5b548c7",
+    "url": "/Slot-machine/static/js/runtime-main.8ae04204.js"
+  },
+  {
+    "revision": "4b182b36cc9570b6bce2029f6415b51f",
+    "url": "/Slot-machine/static/media/pokemon (1).4b182b36.png"
+  },
+  {
+    "revision": "79de74d1180eba07bc81ffa5501d1687",
+    "url": "/Slot-machine/static/media/pokemon (2).79de74d1.png"
+  },
+  {
+    "revision": "7914f6846f43a128c57aaeb598f126c4",
+    "url": "/Slot-machine/static/media/pokemon (3).7914f684.png"
+  },
+  {
+    "revision": "f5351e7336623f09e61272c589fd8a32",
+    "url": "/Slot-machine/static/media/pokemon (4).f5351e73.png"
+  },
+  {
+    "revision": "7517a12a18c8bf9af1a6431533c35acc",
+    "url": "/Slot-machine/static/media/pokemon (5).7517a12a.png"
+  },
+  {
+    "revision": "2b95d4a591455b15fbe361411693465c",
+    "url": "/Slot-machine/static/media/pokemon.2b95d4a5.png"
+  }
+]);
